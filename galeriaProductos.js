69f864fb2c1d6532d7d658d31b12ce5d87a5b1f4
Russo Trv <juanitrovato@gmail.com>
2023-10-17 13:12:39 -0300

    catalogo generado a partir de archivo json

--- galeriaProductos.js
+++ galeriaProductos.js
@@ -1,10 +1,10 @@
 //Funcion para crear la Galeria de Productos
 
 //Funcion para crear TARJETA de PRODUCTO --> CATALOGO
+/*
 let galeria = document.querySelector('.contenedor-galeria');
-
+*/
 function tarjetaProducto(urlFoto, nombre, precio, bajada){
-    let contenedor = document.querySelector('.contenedor-productos');
 
     //Tarjeta que contiene al producto
     let tarjeta = document.createElement('div');
@@ -34,7 +34,7 @@ function tarjetaProducto(urlFoto, nombre, precio, bajada){
 
     let precioProducto = document.createElement('span');
     precioProducto.classList.add("precio");
-    precioProducto.innerHTML = "$" + precio;
+    precioProducto.innerHTML = precio;
 
     cabecera.append(nombreProducto,precioProducto);
 
@@ -46,19 +46,116 @@ function tarjetaProducto(urlFoto, nombre, precio, bajada){
     descripcionContainer.append(cabecera, detalle);
 
     tarjeta.append(fotoProducto, descripcionContainer);
+    /*
     contenedor.appendChild(tarjeta);
     galeria.appendChild(contenedor);
+    */
+    return tarjeta;
 }
 
-
+/*
 tarjetaProducto('https://www.1999.co.jp/itbig24/10249088.jpg', 'Buzo Caps Corp', 15000, 'descripción del buzo');
-
 tarjetaProducto('https://www.1999.co.jp/itbig24/10249088.jpg', 'Buzo Caps Corp', 15000, 'descripción del buzo');
-
 tarjetaProducto('https://www.1999.co.jp/itbig24/10249088.jpg', 'Buzo Caps Corp', 15000, 'descripción del buzo');
-
 tarjetaProducto('https://www.1999.co.jp/itbig24/10249088.jpg', 'Buzo Caps Corp', 15000, 'descripción del buzo');
-
 tarjetaProducto('https://www.1999.co.jp/itbig24/10249088.jpg', 'Buzo Caps Corp', 15000, 'descripción del buzo');
-
 tarjetaProducto('https://www.1999.co.jp/itbig24/10249088.jpg', 'Buzo Caps Corp', 15000, 'descripción del buzo');
+*/
+
+
+/*Cargar Productos al CATALOGO*/
+
+/*Cargar Productos de Porcelana Fria*/
+// Cargar el archivo JSON
+var listaAccesorios;
+var listaTejidos;
+var listaPorcelanaFria;
+
+var rutaCatalogoAccesorios = './catalogo-accesorios.json';
+var galeriaAccesorios = document.querySelector('.contenedor-galeria-accesorios');
+var contenedorAccesorios = document.querySelector('.contenedor-accesorios');
+
+var rutaCatalogoTejido = './catalogo-tejido.json';
+var galeriaTejido = document.querySelector('.contenedor-galeria-tejido');
+var contenedorTejido = document.querySelector('.contenedor-tejido');
+
+var rutaCatalogoPorcelanaFria = './catalogo-porcelana-fria.json';
+var galeriaPorcelanaFria = document.querySelector('.contenedor-galeria-porcelana-fria');
+var contenedorPorcelanaFria = document.querySelector('.contenedor-porcelana-fria');
+
+
+document.addEventListener('DOMContentLoaded', function () {    
+    catalogoTejido(rutaCatalogoTejido);
+    catalogoPorcelanaFria(rutaCatalogoPorcelanaFria);
+    catalogoAccesorios(rutaCatalogoAccesorios);      
+});  
+
+
+function catalogoAccesorios(ruta){
+    fetch(ruta)
+        .then(response => response.json())
+        .then(data => {
+            listaAccesorios = data; // Asignamos el contenido a la variable 
+            listaAccesorios.forEach(producto => {
+                /*console.log(producto);*/
+                cargarAccesorios(producto);
+            });
+    })
+    .catch(error => {
+        console.error('Error al cargar el archivo catalogo-accesorios.json:', error);
+    });
+}
+
+function catalogoPorcelanaFria(ruta){
+    fetch(ruta)
+        .then(response => response.json())
+        .then(data => {
+            listaPorcelanaFria = data; // Asignamos el contenido a la variable 
+            listaPorcelanaFria.forEach(producto => {
+                /*console.log(producto);*/
+                cargarPorcelanaFria(producto);
+            });
+    })
+    .catch(error => {
+        console.error('Error al cargar el archivo catalogo-porcelana-fria.json:', error);
+    });
+}
+
+function catalogoTejido(ruta){
+    fetch(ruta)
+        .then(response => response.json())
+        .then(data => {
+            listaTejidos = data; // Asignamos el contenido a la variable 
+            listaTejidos.forEach(producto => {
+                /*console.log(producto);*/
+                cargarTejidos(producto);
+            });
+    })
+    .catch(error => {
+        console.error('Error al cargar el archivo catalogo-tejido.json:', error);
+    });
+}
+
+function cargarAccesorios(producto){
+    let url = './productos/' + producto.PRODUCTOS;
+    /* tarjetaProducto(urlFoto, nombre, precio, descripcion) */
+    let tarjeta = tarjetaProducto(url, producto.NOMBRE, producto.PRECIO, producto.DESCRIPCION);
+    contenedorAccesorios.appendChild(tarjeta);
+    galeriaAccesorios.appendChild(contenedorAccesorios);
+}
+
+function cargarTejidos(producto){
+    let url = './productos/' + producto.PRODUCTOS;
+    /* tarjetaProducto(urlFoto, nombre, precio, descripcion) */
+    let tarjeta = tarjetaProducto(url, producto.NOMBRE, producto.PRECIO, producto.DESCRIPCION);
+    contenedorTejido.appendChild(tarjeta);
+    galeriaTejido.appendChild(contenedorTejido);
+}
+
+function cargarPorcelanaFria(producto){
+    let url = './productos/' + producto.PRODUCTOS;
+    /* tarjetaProducto(urlFoto, nombre, precio, descripcion) */
+    let tarjeta = tarjetaProducto(url, producto.NOMBRE, producto.PRECIO, producto.DESCRIPCION);
+    contenedorPorcelanaFria.appendChild(tarjeta);
+    galeriaPorcelanaFria.appendChild(contenedorPorcelanaFria);
+}
